299be786fbf2d5518fe71ac239938b0c5ac16dab
Dongwan Seoh <dongwanseoh@gmail.com>
2025-10-30 10:30:58 -0400

    update

--- backend/app/routes/contact_groups.py
+++ backend/app/routes/contact_groups.py
@@ -6,30 +6,5 @@ from app.core.database import supabase_client
 # Routes
 # ---------------------------
 
-def add_new_contact_group_relation(contact_id,group_id):
-    try:
-        response = supabase_client.table("contact_groups").insert({
-            "contact_id":contact_id,
-            "group_id":group_id
-        }).execute()
-        return response.data
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error creating contact: {str(e)}")
-    
-def remove_contact_group_relation(contact_id,group_id):
-    #code here
-    try:
-        response = supabase_client.table("contact_groups") \
-            .delete() \
-            .eq("contact_id", contact_id) \
-            .eq("group_id", group_id) \
-            .execute()
 
-        if not response.data:
-            raise HTTPException(status_code=404, detail="Group not found")
-
-        return {"message": "Group deleted"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error deleting groups: {str(e)}")
-    
 #need to account for when they get rid of the group tag, have to delete
